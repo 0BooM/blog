26e212a30cb0eb3dcd2e31384d0ecc230e8d4fa8
0BooM <149960425+0BooM@users.noreply.github.com>
2025-11-15 14:21:36 +0100

    Implement basic JWT auth

---
+++ src/main/java/com/boom/blog/security/JwtAuthenticationFilter.java
@@ -0,0 +1,59 @@
+package com.boom.blog.security;
+
+
+import com.boom.blog.services.AuthenticationService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Slf4j
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final AuthenticationService authenticationService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        try {
+
+
+            String token = extractToken(request);
+            UserDetails userDetails = null;
+            UsernamePasswordAuthenticationToken authentication = null;
+            if (token != null) {
+                userDetails = authenticationService.validateToken(token);
+
+                authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            }
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            if (userDetails instanceof BlogUserDetails) {
+                request.setAttribute("userId", ((BlogUserDetails) userDetails).getId());
+            }
+        } catch (Exception ex) {
+            // Don't authenticate user
+            log.warn("Received invalid auth token");
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String extractToken(HttpServletRequest request) {
+       String bearerToken = request.getHeader("Authorization");
+       if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+           return bearerToken.substring(7);
+       }
+       return null;
+    }
+}
